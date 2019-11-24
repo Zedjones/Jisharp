01042cc4ace71534208f173e550a6364adbefb96
Zedjones <dojoman19@gmail.com>
2019-11-23 22:35:20 -0500

    Very initial work on JishoClient

---
+++ JishoClient.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Net.Http;
+
+namespace jisharp
+{
+    public class JishoClient
+    {
+        const string BASE_URL = "https://jisho.org/api/v1/search/";
+        HttpClient client;
+
+        public JishoClient()
+        {
+            this.client = new HttpClient();
+        }
+    }
+}
