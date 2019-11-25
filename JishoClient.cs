c72c6908786d8ed514c90e5697d5c2b85081ba4e
Zedjones <dojoman19@gmail.com>
2019-11-25 11:52:00 -0500

    Started working on SearchWord

--- JishoClient.cs
+++ JishoClient.cs
@@ -1,16 +1,30 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Net.Http;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace jisharp
 {
     public class JishoClient
     {
-        const string BASE_URL = "https://jisho.org/api/v1/search/";
+        const string BASE_URL = "https://jisho.org/api/v1/search/words?keyword={0}";
         HttpClient client;
 
         public JishoClient()
         {
             this.client = new HttpClient();
+        }
+        public async Task<Dictionary<string, dynamic>> SearchWord(string word) {
+            var resp = await client.GetAsync(string.Format(BASE_URL, word));
+            var wordDict = JsonConvert.DeserializeObject<Dictionary<string, dynamic>>(await resp.Content.ReadAsStringAsync());
+            return wordDict;
+        }
+        static void Main(string[] args) {
+            var client = new JishoClient();
+            var searchTask = client.SearchWord("iie");
+            searchTask.Wait();
+            Console.WriteLine(searchTask.Result);
         }
     }
 }
